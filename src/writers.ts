c28f584908d7b8db4ec44f5cde0d6d11b747f2fe
Scott Piriou <30843220+pscott@users.noreply.github.com>
2025-03-18 17:19:49 +0100

    feat: add space entity and turbo expiration computation

--- src/writers.ts
+++ src/writers.ts
@@ -1,10 +1,40 @@
 import { evm } from '@snapshot-labs/checkpoint';
-import { Payment } from '../.checkpoint/models';
+import { Payment, Space } from '../.checkpoint/models';
 import { getJSON } from './utils';
 import tokens from './payment_tokens.json';
 
+const TURBO_MONTHLY_PRICE = BigInt(0.1 * 1e6); // 10 USDC
+const TURBO_YEARLY_PRICE = BigInt(0.8 * 1e6); // 100 USDC
+
+const SNAPSHOT_ADDRESS = '0x1234';
+
 function getTokenSymbol(tokenAddress: string, chain: string) {
   return tokens[chain][tokenAddress];
+}
+
+function computeExpiration(space: Space, payment: Payment, metadata: any, blockTimestamp: number) {
+  // If the payment is from the snapshot address, simply return the expiration date from the metadata
+  if (payment.sender.toLowerCase() === SNAPSHOT_ADDRESS) {
+    return new Date(metadata.params.expiration * 1000);
+  }
+
+  const currentexpirationTimestamp = space.turbo_expiration_timestamp
+    ? space.turbo_expiration_timestamp
+    : blockTimestamp;
+  const expirationDate = new Date(currentexpirationTimestamp * 1000); // Multiply by 1000 to convert to milliseconds
+
+  if (payment.amount_raw < TURBO_MONTHLY_PRICE) {
+    // Return early because the payment is not enough to extend the expiration
+    return null;
+  }
+
+  const months = Number((payment.amount_raw % TURBO_YEARLY_PRICE) / TURBO_MONTHLY_PRICE);
+  const years = Number(payment.amount_raw / TURBO_YEARLY_PRICE);
+
+  expirationDate.setMonth(expirationDate.getMonth() + months);
+  expirationDate.setFullYear(expirationDate.getFullYear() + years);
+
+  return expirationDate;
 }
 
 export function createEvmWriters(indexerName: string) {
@@ -36,6 +66,20 @@ export function createEvmWriters(indexerName: string) {
     payment.timestamp = block.timestamp;
 
     await payment.save();
+
+    // Try to get the space entity
+    let space = await Space.loadEntity(metadata.params.space, indexerName);
+    // If it doesn't exist, create it
+    if (!space) {
+      space = new Space(metadata.params.space, indexerName);
+    }
+
+    const expirationDate = computeExpiration(space, payment, block.timestamp, metadata);
+    if (expirationDate !== null) {
+      space.turbo_expiration_timestamp = expirationDate.getTime() / 1000; // Divide by 1000 to convert to seconds
+      space.turbo_expiration_date = expirationDate.toDateString();
+      await space.save();
+    }
   };
 
   return {
