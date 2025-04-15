f06f811abfcb65149841a539de5e23f7804e0d29
pscott <30843220+pscott@users.noreply.github.com>
2025-04-15 05:06:14 +0200

    feat: allow user to deposit any amount and set expiration date accordingly (#2)
    * refactor: remove base support
    
    * feat: add space entity and turbo expiration computation
    
    * refactor: allow user to deposit more than monthly / yearly price and update expiration date based on price per second
    
    * chore: use MILLISECONDS const instead of 1000
    
    * refactor: add default ADMIN address
    
    * refactor: use Space instead of beneficiary
    
    * typo: proper casing on currentExpirationTimestamp
    
    Co-authored-by: Less <bonustrack@users.noreply.github.com>
    
    * chore: update yarn.lock
    
    * chore: add CA_CERT code
    
    * refactor: unlink payments and space
    
    * chore: use ?? ternary
    
    * refactor: have computeExpiration return a Date
    
    * fix: check if turbo_expiration_timestamp is zero
    
    * chore: rename to turbo_expiration
    
    * refactor: add price as env vars
    
    * fix: set expiration to 0 only if user did not already have an expiration date
    
    * fix: correctly use decimals for turbo prices
    
    * refactor: hardcode turbo price
    
    * fix: handle discontinued payment case
    
    * chore: remove dead console.log
    
    * chore: use max of turbo_expiration and blockTimestamp
    
    Co-authored-by: Wan <495709+wa0x6e@users.noreply.github.com>
    
    * typo: math.max
    
    ---------
    
    Co-authored-by: Less <bonustrack@users.noreply.github.com>
    Co-authored-by: Wan <495709+wa0x6e@users.noreply.github.com>

--- src/writers.ts
+++ src/writers.ts
@@ -1,21 +1,91 @@
 import { evm } from '@snapshot-labs/checkpoint';
-import { Payment } from '../.checkpoint/models';
+import { Payment, Space } from '../.checkpoint/models';
 import { getJSON } from './utils';
 import tokens from './payment_tokens.json';
+
+const MILLISECONDS = 1000;
+const DECIMALS = 1e6; // USDC and USDT both have 6 decimals
+
+const TURBO_MONTHLY_PRICE = 600 * DECIMALS;
+const TURBO_YEARLY_PRICE = 6000 * DECIMALS;
+
+const DAYS_PER_YEAR = (365 * 3 + 366) / 4; // Accounting for leap years, which happens even four year (this is technically incorrect due to leap seconds but it's good enough for this purpose)
+const YEARLY_PRICE_PER_DAY = TURBO_YEARLY_PRICE / DAYS_PER_YEAR;
+const YEARLY_PRICE_PER_SECOND = YEARLY_PRICE_PER_DAY / (24 * 60 * 60); // 24 hours * 60 minutes * 60 seconds
+
+const DAYS_PER_MONTH = (365 * 3 + 366) / 48; // Accounting for leap years, which happens even four year (this is technically incorrect due to leap seconds but it's good enough for this purpose)
+const MONTHLY_PRICE_PER_DAY = TURBO_MONTHLY_PRICE / DAYS_PER_MONTH;
+const MONTHLY_PRICE_PER_SECOND = MONTHLY_PRICE_PER_DAY / (24 * 60 * 60); // 24 hours * 60 minutes * 60 seconds
+
+const ADMIN_ADDRESS = process.env.ADMIN_ADDRESS || '0x8C28Cf33d9Fd3D0293f963b1cd27e3FF422B425c';
 
 function getTokenSymbol(tokenAddress: string, chain: string) {
   return tokens[chain][tokenAddress];
 }
 
+// Computes the time of expiration of a space based on the payment received.
+// The logic is as follows:
+// - Returns `null` if the payment is not enough to extend the by at least one month
+// - If the user has paid for more than a year, the expiration date is extended by the number of years paid
+//   - Then take the surplus and increase the expiration date based on the YEARLY_PRICE_PER_SECOND
+// - If the user has paid for more than a month (but less than a year), the expiration date is extended by the number of months paid
+//   - Then take the surplus and increase the expiration date based on the MONTHLY_PRICE_PER_SECOND
+function computeExpiration(
+  space: Space,
+  payment: Payment,
+  metadata: any,
+  blockTimestamp: number
+): Date {
+  // If the payment is from the admin address, simply return the expiration date from the metadata
+  if (payment.sender.toLowerCase() === ADMIN_ADDRESS) {
+    return new Date(metadata.params.expiration * MILLISECONDS);
+  }
+
+  if (payment.amount_raw < TURBO_MONTHLY_PRICE) {
+    // Return early because the payment is not enough to extend the expiration
+    if (space.turbo_expiration) {
+      // User already had an expiration date, leave it untouched.
+      return new Date(space.turbo_expiration * 1000);
+    } else {
+      // User didn't have an expiration date, leave it to 0
+      return new Date(0);
+    }
+  }
+
+  // If the space already has an expiration date, use it as the current expiration date
+  const currentExpirationTimestamp = Math.max(space.turbo_expiration, blockTimestamp);
+  const expirationDate = new Date(currentExpirationTimestamp * MILLISECONDS); // Multiply by 1000 to convert to milliseconds
+
+  const userPaidAtLeastAYear = payment.amount_raw >= TURBO_YEARLY_PRICE;
+  if (userPaidAtLeastAYear) {
+    const years = Number(payment.amount_raw) / TURBO_YEARLY_PRICE;
+    expirationDate.setFullYear(expirationDate.getFullYear() + years);
+
+    const surplus = Number(payment.amount_raw) % TURBO_YEARLY_PRICE;
+    const surplusSeconds = surplus / YEARLY_PRICE_PER_SECOND;
+    expirationDate.setSeconds(expirationDate.getSeconds() + surplusSeconds);
+  } else if (payment.amount_raw >= TURBO_MONTHLY_PRICE) {
+    const months = Number(payment.amount_raw) / TURBO_MONTHLY_PRICE;
+    expirationDate.setMonth(expirationDate.getMonth() + months);
+
+    const surplus = Number(payment.amount_raw) % TURBO_MONTHLY_PRICE;
+    const surplusSeconds = surplus / MONTHLY_PRICE_PER_SECOND;
+    expirationDate.setSeconds(expirationDate.getSeconds() + surplusSeconds);
+  } else {
+    console.log('error, unreachable code. Payment is not enough to extend the expiration');
+  }
+
+  return expirationDate;
+}
+
 export function createEvmWriters(indexerName: string) {
   const handlePaymentReceived: evm.Writer = async ({ block, tx, event }) => {
-    console.log('In handlePaymentReceived');
     if (!block || !event) return;
 
     const sender = event.args.sender;
     const tokenAddress = event.args.token.toLowerCase();
     const amountRaw = BigInt(event.args.amount);
-    const amountDecimal = Number(amountRaw) / 1e6;
+    const amountDecimal = Number(amountRaw) / DECIMALS;
     const barcode = event.args.barcode;
 
     const tokenSymbol = getTokenSymbol(tokenAddress, indexerName) || '';
@@ -32,10 +102,28 @@ export function createEvmWriters(indexerName: string) {
 
     payment.block = block.number;
     payment.type = metadata.type;
-    payment.beneficiary = payment.type === 'turbo' ? metadata.params.space : 'unknown';
+
+    if (payment.type === 'turbo') {
+      payment.space = metadata.params.space;
+    }
+
     payment.timestamp = block.timestamp;
 
     await payment.save();
+
+    // Try to get the space entity
+    let space = await Space.loadEntity(metadata.params.space, indexerName);
+    // If it doesn't exist, create it
+    if (!space) {
+      space = new Space(metadata.params.space, indexerName);
+    }
+
+    const expirationDate = computeExpiration(space, payment, metadata, block.timestamp);
+
+    space.turbo_expiration = expirationDate.getTime() / MILLISECONDS; // Divide by 1000 to convert to seconds
+    space.turbo_expiration_date = expirationDate.toDateString();
+
+    await space.save();
   };
 
   return {
