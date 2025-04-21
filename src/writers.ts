f1c2c4e2afb43311128cb7cfc221c6ac28136011
Scott Piriou <30843220+pscott@users.noreply.github.com>
2025-04-21 15:33:00 +0200

    fix: treat admin payment as normal payment if amount is > 0

--- src/writers.ts
+++ src/writers.ts
@@ -39,7 +39,7 @@ function computeExpiration(
   blockTimestamp: number
 ): Date {
   // If the payment is from the admin address, simply return the expiration date from the metadata
-  if (payment.sender === ADMIN_ADDRESS) {
+  if (payment.sender === ADMIN_ADDRESS && payment.amount_raw == 0n) {
     const date = new Date(metadata.params.expiration * MILLISECONDS);
     if (isNaN(date.getTime())) {
       return new Date(0);
