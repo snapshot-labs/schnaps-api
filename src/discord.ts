c136aa32b5d122d42824e448a928acaf953541b7
Less <bonustrack@users.noreply.github.com>
2025-05-02 04:14:16 +0700

    Update src/discord.ts

--- src/discord.ts
+++ src/discord.ts
@@ -18,7 +18,7 @@ export async function notifyPayment(
 
   const content = `**New payment of ${payment.amount_decimal} ${payment.token_symbol} for [${
     payment.space
-  }](https://${INDEX_TESTNET ? 'testnet.' : ''}snapshot.box/#/s${INDEX_TESTNET ? '-sn.' : ''}:${
+  }](https://${INDEX_TESTNET ? 'testnet.' : ''}snapshot.box/#/s${INDEX_TESTNET ? '-tn' : ''}:${
     payment.space
   })**\nFrom [${payment.sender}](https://${INDEX_TESTNET ? 'sepolia.' : ''}etherscan.io/address/${
     payment.sender
