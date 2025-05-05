51f037e8137daf3ddb7c5b3592efe89e6f1a5a09
less <fabien@bonustrack.co>
2025-05-06 04:37:34 +0700

    chore: move to Discord embeds

--- src/discord.ts
+++ src/discord.ts
@@ -12,24 +12,47 @@ export async function notifyPayment(
   if (!DISCORD_WEBHOOK_URL) return;
 
   const now = ~~(Date.now() / 1e3);
-  const recentThreshold = now - 60 * 30;
+  const recentThreshold = now - 60 * 60; // 1 hour
 
   if (block.timestamp < recentThreshold) return;
 
-  const content = `**New payment of ${payment.amount_decimal} ${payment.token_symbol} for [${
-    payment.space
-  }](https://${INDEX_TESTNET ? 'testnet.' : ''}snapshot.box/#/${payment.space})**\nFrom [${
-    payment.sender
-  }](https://${INDEX_TESTNET ? 'sepolia.' : ''}etherscan.io/address/${
-    payment.sender
-  }), expiration : ${space.turbo_expiration_date}\n<https://${
-    INDEX_TESTNET ? 'sepolia.' : ''
-  }etherscan.io/tx/${tx.hash}>`;
+  const explorerBaseUrl = `https://${INDEX_TESTNET ? 'sepolia.' : ''}etherscan.io`;
+  const snapshotBaseUrl = `https://${INDEX_TESTNET ? 'testnet.' : ''}snapshot.box`;
 
   await fetch(DISCORD_WEBHOOK_URL, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ content })
+    body: JSON.stringify({
+      embeds: [
+        {
+          title: `💰 New payment of ${payment.amount_decimal} ${payment.token_symbol}`,
+          url: `${explorerBaseUrl}/tx/${tx.hash}`,
+          author: {
+            name: payment.sender,
+            icon_url: `https://cdn.stamp.fyi/avatar/${payment.sender}`,
+            link: `${explorerBaseUrl}/address/${payment.sender}`
+          },
+          fields: [
+            {
+              name: 'Space',
+              value: `[${payment.space}](${snapshotBaseUrl}/#/${payment.space})`,
+              inline: true
+            },
+            {
+              name: 'Network',
+              value: !INDEX_TESTNET ? 'Ethereum' : 'Sepolia',
+              inline: true
+            },
+            {
+              name: 'Expiration',
+              value: `<t:${space.turbo_expiration}:R>`,
+              inline: true
+            }
+          ],
+          timestamp: new Date(block.timestamp * 1000).toISOString()
+        }
+      ]
+    })
   });
 
   return;
